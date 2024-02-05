d51b5b885a2596e77d65719cd17dcf2148994c20
Davodii <david.adrian.ilie2005@gmail.com>
2024-02-05 12:31:43 +0000

    still working on code

---
+++ Assets/TeacherCardUI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UI;
+using UnityEngine;
+
+public class TeacherCardUI : MonoBehaviour
+{
+    [SerializeField] private ItemUIGroup _itemUI;
+}
